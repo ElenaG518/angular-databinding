a2de8d37874b5d53aa9537bb2e25adc8f7cd03f5
Elena Granados <pachimac@Elenas-iMac.local>
2019-07-08 17:17:54 -0700

    continuation of angular app

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,16 +1,72 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewEncapsulation,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+  ViewChild,
+  ElementRef,
+  ContentChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated //None, Native
 })
 export class ServerElementComponent implements OnInit {
-  @Input('srvElement') element: {type: string, name: string; content: string};
+  @Input('srvElement') element: { type: string; name: string; content: string };
+  @Input() name: string;
+  @ViewChild('heading') header: ElementRef;
+  @ContentChild('contentParagraph') paragraph: ElementRef;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    console.log('constructor');
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges');
+    console.log(changes);
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit');
+    console.log('Text content', this.header.nativeElement.textContent);
+    console.log('Content paragraph', this.paragraph.nativeElement.textContent);
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit() {
+    console.log('AfterContentInit');
+    console.log('Content paragraph', this.paragraph.nativeElement.textContent);
+  }
+
+  ngAfterContentChecked() {
+    console.log('AfterContentChecked');
+  }
+
+  // can only render template elements and access the value of elements in DOM's in AfterViewInit, but not befores
+  ngAfterViewInit() {
+    console.log('AfterViewInit');
+    console.log('Text content', this.header.nativeElement.textContent);
+  }
+
+  ngAfterViewChecked() {
+    console.log('AfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    console.log('onDestroy');
+  }
 }
